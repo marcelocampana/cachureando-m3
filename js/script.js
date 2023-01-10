55ae4dd666e90610f82a8cc8c5bda3a93aa229f5
Jozz16 <jo633554@gmail.com>
2023-01-09 21:36:55 -0300

    objetos

---
+++ js/script.js
@@ -0,0 +1,63 @@
+
+let shoppingCart = [
+    {img:"a",
+    name:'clarity',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+    {img:'a',
+    name:'Somos polvo de estrellas',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+    {img:'a',
+    name:'Marte: la próxima frontera',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+    {img:'a',
+    name:'Eclipses, Planeta',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+    {img:'a',
+    name:'Bajo el manto de Urania',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+    {img:'a',
+    name:'La teoria del todo',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+    {img:'a',
+    name:'it',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+    {img:'a',
+    name:'El resplandor',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+    {img:'a',
+    name:'joyland',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+    {img:'a',
+    name:'cell',
+    codeproduct:'c',
+    descript: 'd',
+    cost:'e'
+}
+]
