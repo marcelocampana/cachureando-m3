9de8bd78298303e56bcef9f89ad0a37e65a19291
Marcelo Campaña <46510921+marcelocampana@users.noreply.github.com>
2023-01-11 21:52:07 -0300

    js de carrito de compra

--- js/app.js
+++ js/app.js
@@ -20,18 +20,54 @@ function showDetail(codeBook) {
 
   const detailProduct = `<div class="row">
   <div class="col-6">
+
     <img src="${productFiltered[0].cover}" alt=" " class="w-100">
   </div>
   <div class="col-4 m-5">
     <div>${productFiltered[0].titleBook}</div>
     <div>${productFiltered[0].description}</div>
     <div>$${productFiltered[0].price}</div>
-    <button type="button" class="btn btn-primary mt-5">Agregar al carrito</button>
+    <input type="number" id="quantity"/>
+    <button type="button" onclick="addtoCart(${productFiltered[0].codeBook})" class="btn btn-primary mt-5">Agregar al carrito</button>
   </div>
+  <button onclick="showGridProducts()" class="btn btn-primary">Seguir comprando</button>
 </div>`;
   console.log(productFiltered);
 
   document.getElementById("app").innerHTML = detailProduct;
 }
 
-const productoCard1 = productsData.map((element) => element.price);
+//let productsSelected = [];
+function addtoCart(codeBook) {
+  const quantity = document.getElementById("quantity").value;
+  const productSelected = productsData
+    .filter((product) => product.codeBook == codeBook)
+    .map((product) => ({
+      title: product.titleBook,
+      price: product.price,
+      queantity: quantity,
+    }));
+
+  /* const productSelectedPrice = productsData
+    .filter((product) => product.codeBook == codeBook)
+    .map((product) => product.price); */
+
+  //productSelected.push(quantity, productSelectedPrice[0]);
+  //productSelected.push();
+  //productSelected.push(productSelectedPrice);
+
+  console.log(productSelected);
+
+  //const multiplication = productSelected[1] * productSelected[2];
+
+  const cart = productSelected.map(
+    () =>
+      `<div>${productSelected[0].queantity * productSelected[0].price}</div>`
+  );
+
+  document.getElementById("cart").innerHTML += cart;
+}
+
+function showGridProducts() {
+  document.getElementById("app").innerHTML = productoCard;
+}
