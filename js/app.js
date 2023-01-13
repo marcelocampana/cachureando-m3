5310937931850faf47570fa0452d1bdfe65f0846
Marcelo Campaña <46510921+marcelocampana@users.noreply.github.com>
2023-01-12 21:17:30 -0300

    valores de carrito agregados

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ document.getElementById("product-grid").innerHTML = productCard;
 
 function showDetail(codeBook) {
   document.getElementById("product-grid").className = "d-none";
+  document.getElementById("product-detail").className = "d-block";
   const productFiltered = productsData.filter(
     (product) => product.codeBook == codeBook
   );
@@ -40,13 +41,16 @@ function showDetail(codeBook) {
 
 //let productsSelected = [];
 function addtoCart(codeBook) {
+  document.getElementById("product-cart").className = "d-block";
+  document.getElementById("product-detail").className = "d-none";
   const quantity = document.getElementById("quantity").value;
   const productSelected = productsData
     .filter((product) => product.codeBook == codeBook)
     .map((product) => ({
       title: product.titleBook,
       price: product.price,
-      queantity: quantity,
+      quantity,
+      cover: product.cover,
     }));
 
   /* const productSelectedPrice = productsData
@@ -59,35 +63,21 @@ function addtoCart(codeBook) {
 
   console.log(productSelected);
 
-  //const multiplication = productSelected[1] * productSelected[2];
-
-  const cart = productSelected.map(
-    () =>
-      `<div>${productSelected[0].queantity * productSelected[0].price}</div>`
-  );
-
-  document.getElementById("product-cart").innerHTML += cart;
-}
-
-function showGridProducts() {
-  document.getElementById("product-cart").innerHTML = productCard;
-}
-
-const carrito = ` 
+  const cartHtml = ` 
 <section class="h-100" style="background-color: #eee;">
-<div class="container h-100 py-5">
+<div class="container h-100 py-1">
   <div class="row d-flex justify-content-center align-items-center h-100">            
-      <div class="card rounded-3 mb-4">
+      <div class="card rounded-3 ">
         <div class="card-body p-4">
           <div class="row d-flex justify-content-between align-items-center">
             <div class="col-md-2 col-lg-2 col-xl-2">
               <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img1.webp"
-                class="img-fluid rounded-3" alt="Cotton T-shirt">
+                src="${productSelected[0].cover}"
+                class="img-fluid rounded-3" alt="${productSelected[0].titleBook}">
             </div>
             <div class="col-md-3 col-lg-3 col-xl-3">
-              <p class="lead fw-normal mb-2">Basic T-shirt</p>
-              <p><span class="text-muted">Size: </span>M <span class="text-muted">Color: </span>Grey</p>
+              <p class="lead fw-normal mb-2">${productSelected[0].title}</p>
+         
             </div>
             <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
               <button class="btn btn-link px-2"
@@ -95,7 +85,7 @@ const carrito = `
                 <i class="fas fa-minus"></i>
               </button>
 
-              <input id="form1" min="0" name="quantity" value="2" type="number"
+              <input id="form1" min="0" name="quantity" value="${productSelected[0].quantity}" type="number"
                 class="form-control form-control-sm" />
 
               <button class="btn btn-link px-2"
@@ -104,8 +94,8 @@ const carrito = `
               </button>
             </div>
             <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-              <h5 class="mb-0">$499.00</h5>
-            </div>
+              <h5 class="mb-0">$ ${productSelected[0].price}</h5>      
+            </div>        
             <div class="col-md-1 col-lg-1 col-xl-1 text-end">
               <a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>
             </div>
@@ -116,4 +106,19 @@ const carrito = `
   </div>
 </div>
 </section>`;
-document.getElementById("cart").innerHTML = carrito;
+  productSelected.map(
+    () => (document.getElementById("product-cart").innerHTML += cartHtml)
+  );
+}
+
+const btnBack = document.createElement("button");
+btnBack.className = "btn btn-primary m-3";
+btnBack.setAttribute("onclick", "showGridProducts()");
+btnBack.textContent = "Seguir Comprando";
+
+document.getElementById("product-cart").appendChild(btnBack);
+
+function showGridProducts() {
+  document.getElementById("product-grid").className = "row";
+  document.getElementById("product-cart").className = "d-none";
+}
