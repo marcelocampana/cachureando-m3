bed7ab96bf273bd549d5ec7e15fe55f30e9096a0
Marcelo Campaña <46510921+marcelocampana@users.noreply.github.com>
2023-01-14 23:15:46 -0300

    funcion para eliminar items del carrito ok!

--- js/app.js
+++ js/app.js
@@ -1,44 +1,59 @@
+//Obtener elementos HTML
 const DOMgrid = document.getElementById("product-grid");
+const DOMdetail = document.getElementById("product-detail");
+const DOMcart = document.getElementById("product-cart");
+
+/* ------------GRILLA DE PRODUCTOS----------------------------------- */
 
 function createProductGrid(title, image, price, code) {
-  // Tamano de las card
+  // Tamaño de las card
   const wrap = document.createElement("div");
+  wrap.classList.add("col-6", "col-md-3", "col-lg-2");
 
   //Card de producto
   const card = document.createElement("div");
+  card.classList.add("card", "mb-3", "w-100");
   card.setAttribute("id", "card");
   card.style.height = "480px";
 
   //Imagen de la card
   const imageCard = document.createElement("img");
+  imageCard.classList.add("card-img-top", "img-fluid");
   imageCard.style.maxHeight = "24rem";
   imageCard.src = image;
   imageCard.alt = title;
 
   //Cuerpo de la card
   const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
 
   //Titulo de la card
   const cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title");
   cardTitle.innerText = title;
 
   //Precio de la card
   const cardPrice = document.createElement("p");
-  cardPrice.innerText = `$${price}`;
+  cardPrice.classList.add("card-text");
+  cardPrice.innerText = "$" + Intl.NumberFormat("es-CL").format(price);
 
   //boton de la card
   const btn = document.createElement("button");
-  btn.setAttribute("onclick", `showDetail(${code})`);
+  btn.classList.add("btn", "btn-primary");
+  btn.setAttribute("onclick", `displayProductDetail(${code})`);
   btn.innerText = "Comprar";
 
-  //agregando elementos al DOM y asignadoles clases
-  DOMgrid.appendChild(wrap).className = "col-6 col-md-3 col-lg-2";
-  wrap.appendChild(card).className = "card  mb-3 w-100";
-  card.appendChild(imageCard).className = "card-img-top img-fluid";
-  card.appendChild(cardBody).className = "card-body";
-  cardBody.appendChild(cardTitle).className = "card-title";
-  cardBody.appendChild(cardPrice).className = "card-text";
-  cardBody.appendChild(btn).className = "btn btn-primary";
+  //Agregando elementos al DOM
+  DOMgrid.appendChild(wrap);
+  wrap.appendChild(card);
+  card.appendChild(imageCard);
+  card.appendChild(cardBody);
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardPrice);
+  cardBody.appendChild(btn);
+
+  //Mostrando solo elementos dentro del id #product-grid
+  showStoreComponent("product-grid");
 }
 
 const productGrid = productsData.map((product) =>
@@ -49,3 +64,231 @@ const productGrid = productsData.map((product) =>
     product.codeBook
   )
 );
+
+/* ------------DETALLE DE PRODUCTO----------------------------------- */
+
+//Creando componente detalle de producto
+function createProductDetail(code, title, imageUrl, price, description) {
+  //Eliminar todo el contenido
+  DOMdetail.textContent = "";
+  //Div inicial
+  const wrapDetail = document.createElement("div");
+  wrapDetail.classList.add("col-6", "d-flex", "justify-content-center");
+  //Imagen
+  const imageDetail = document.createElement("img");
+  imageDetail.src = imageUrl;
+  imageDetail.alt = title;
+  imageDetail.classList.add("h-75");
+  //Cuerpo
+  const bodyDetail = document.createElement("div");
+  bodyDetail.classList.add("col-4", "mx-5");
+  //Titulo
+  const detailTitle = document.createElement("h1");
+  detailTitle.innerText = title;
+  //Descripcion
+  const detailDescription = document.createElement("div");
+  detailDescription.innerText = description;
+  //Precio
+  const detailPrice = document.createElement("h5");
+  detailPrice.classList.add("text-left", "pt-4", "pb-4");
+  detailPrice.innerText = "$" + Intl.NumberFormat("es-CL").format(price);
+  //Disponibilidad
+  const detailAvailability = document.createElement("h5");
+  detailAvailability.classList.add("pb-4");
+  detailAvailability.innerText = "Disponibilidad: en stock";
+  //input de cantidad
+  const detailQuantityTitle = document.createElement("h6");
+  detailQuantityTitle.innerText = "Cantidad";
+  const detailQuantityInput = document.createElement("input");
+  detailQuantityInput.setAttribute("type", "number");
+  detailQuantityInput.setAttribute("min", "1");
+  detailQuantityInput.setAttribute("value", "1");
+  detailQuantityInput.setAttribute("id", "quantity");
+  //Boton enviar al carrito
+  const detailSendToCartBtn = document.createElement("button");
+  detailSendToCartBtn.classList.add(
+    "btn",
+    "btn-primary",
+    "btn-lg",
+    "mt-3",
+    "d-flex",
+    "justify-content-end"
+  );
+
+  detailSendToCartBtn.setAttribute("onclick", `addProductToCart(${code})`);
+  detailSendToCartBtn.innerText = "Agregar al Carrito";
+  //Boton continuar comprando
+  const detailBtnBack = document.createElement("button");
+  detailBtnBack.setAttribute("type", "button");
+  detailBtnBack.setAttribute("onclick", "showStoreComponent('product-grid')");
+  detailBtnBack.classList.add("btn", "btn-outline-primary", "mt-5");
+  detailBtnBack.innerText = "Continuar comprando";
+
+  //Agregando elementos al DOM
+  DOMdetail.appendChild(wrapDetail);
+  wrapDetail.appendChild(imageDetail);
+  DOMdetail.appendChild(bodyDetail);
+  bodyDetail.appendChild(detailTitle);
+  bodyDetail.appendChild(detailDescription);
+  bodyDetail.appendChild(detailPrice);
+  bodyDetail.appendChild(detailQuantityTitle);
+  bodyDetail.appendChild(detailQuantityInput);
+  bodyDetail.appendChild(detailSendToCartBtn);
+  bodyDetail.appendChild(detailBtnBack);
+}
+
+//mostrar el producto seleccionado
+function displayProductDetail(codeBook) {
+  const productSelected = productsData.filter(
+    (product) => product.codeBook == codeBook
+  );
+
+  createProductDetail(
+    productSelected[0].codeBook,
+    productSelected[0].titleBook,
+    productSelected[0].cover,
+    productSelected[0].price,
+    productSelected[0].description
+  );
+  showStoreComponent("product-detail");
+}
+
+/* ------------CARRITO DE COMPRA----------------------------------- */
+
+function createProductCart(code, title, imageUrl, price, quantity) {
+  const cartWrap = document.createElement("div");
+
+  cartWrap.classList.add("container", "h-100", "py-1");
+
+  //Container de cada producto agregado al carrito(este elemento contiene el #id )
+  const cartContainer = document.createElement("div");
+  cartContainer.setAttribute("id", code);
+  cartContainer.classList.add(
+    "row",
+    "d-flex",
+    "justify-content-center",
+    "align-items-center",
+    "h-100"
+  );
+  //contruyendo la card de cada producto del carrito
+  const cartCard = document.createElement("div");
+  cartCard.classList.add("card", "rounded-3");
+  const cartCardBody = document.createElement("div");
+  cartCardBody.classList.add("card-body", "p-4");
+  const cartCardBodyRow = document.createElement("div");
+  cartCardBodyRow.classList.add(
+    "row",
+    "d-flex",
+    "justify-content-between",
+    "align-items-center"
+  );
+  //Imagen
+  const cartImageCol = document.createElement("div");
+  cartImageCol.classList.add("col-1");
+  const cartImage = document.createElement("img");
+  cartImage.classList.add("img-fluid", "rounded-3");
+  cartImage.src = imageUrl;
+  cartImage.alt = title;
+  //Titulo
+  const cartTitleCol = document.createElement("div");
+  cartTitleCol.classList.add("col-2");
+  const cartTitle = document.createElement("div");
+  cartTitle.classList.add("lead", "fw-normal");
+  cartTitle.innerText = title;
+  //Cantidad
+  const cartQuantityCol = document.createElement("div");
+  cartQuantityCol.classList.add("col-1", "d-flex");
+  const cartQuantity = document.createElement("div");
+  cartQuantity.innerText = quantity;
+  //Precio
+  const cartPriceCol = document.createElement("div");
+  cartPriceCol.classList.add("col-2", "offset-1");
+  const cartPrice = document.createElement("div");
+  cartPrice.classList.add("mb-0");
+  cartPrice.innerText = "$" + Intl.NumberFormat("es-CL").format(price);
+  //Total
+  const cartTotalCol = document.createElement("div");
+  cartTotalCol.classList.add("col-2", "offset-1");
+  const cartTotal = document.createElement("div");
+  cartTotal.classList.add("mb-0");
+  cartTotal.innerText =
+    "$" + Intl.NumberFormat("es-CL").format(price * quantity);
+  const cartBtnDeleteCol = document.createElement("div");
+  //Boton eliminar (Elimina un producto del carrito llamando a la funcion deleteProductFromCard())
+  cartBtnDeleteCol.classList.add(
+    "col-md-1",
+    "col-lg-1",
+    "col-xl-1",
+    "text-end"
+  );
+  const cartBtnDelete = document.createElement("button");
+  cartBtnDelete.setAttribute("onclick", `deleteProductFromCard(${code})`);
+  cartBtnDelete.classList.add(
+    "text-danger",
+    "fs-6",
+    "bg-transparent",
+    "border-0"
+  );
+  cartBtnDelete.innerText = "Eliminar";
+
+  //Agregando elementos al DOM
+  DOMcart.appendChild(cartContainer);
+  cartContainer.appendChild(cartWrap);
+  cartWrap.appendChild(cartCard);
+  cartCard.appendChild(cartCardBody);
+  cartCardBody.appendChild(cartCardBodyRow);
+  cartCardBodyRow.appendChild(cartImageCol);
+  cartImageCol.appendChild(cartImage);
+  cartCardBodyRow.appendChild(cartTitleCol);
+  cartTitleCol.appendChild(cartTitle);
+  cartCardBodyRow.appendChild(cartQuantityCol);
+  cartQuantityCol.appendChild(cartQuantity);
+  cartCardBodyRow.appendChild(cartPriceCol);
+  cartPriceCol.appendChild(cartPrice);
+  cartCardBodyRow.appendChild(cartTotalCol);
+  cartTotalCol.appendChild(cartTotal);
+  cartCardBodyRow.appendChild(cartBtnDeleteCol);
+  cartBtnDeleteCol.appendChild(cartBtnDelete);
+  showStoreComponent("product-cart");
+}
+
+//Agregar producto al carrito
+function addProductToCart(codeBook) {
+  const quantity = document.getElementById("quantity").value;
+
+  const productAddedToCart = productsData
+    .filter((product) => product.codeBook == codeBook)
+    .map((product) => ({
+      codeBook: product.codeBook,
+      titleBook: product.titleBook,
+      price: product.price,
+      quantity,
+      cover: product.cover,
+    }));
+
+  createProductCart(
+    productAddedToCart[0].codeBook,
+    productAddedToCart[0].titleBook,
+    productAddedToCart[0].cover,
+    productAddedToCart[0].price,
+    productAddedToCart[0].quantity
+  );
+}
+
+//Eliminar producto del carrito. Esta funcion busca el elemento que contiene el id que es igual al código de producto y elimina su hijo(child) con removeChild
+function deleteProductFromCard(codeBook) {
+  const itemToRemove = document.getElementById(codeBook);
+  DOMcart.removeChild(itemToRemove);
+}
+
+/* ------------FUNCION PARA MOSTRAR/OCULTAR ----------------------------------- */
+
+//Mostrar/ocultar elementos de asociados al id #product-grid, #product-detail o #product-cart
+function showStoreComponent(component) {
+  document.getElementById("product-grid").className =
+    component === "product-grid" ? "row" : "d-none";
+  document.getElementById("product-detail").className =
+    component === "product-detail" ? "row" : "d-none";
+  document.getElementById("product-cart").className =
+    component === "product-cart" ? "row" : "d-none";
+}
